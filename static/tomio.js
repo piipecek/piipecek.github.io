f805651e98c4c83659e81289987e7c73acf6ab60
josef.latj <josef.latj@gmail.com>
2022-02-24 14:36:24 +0100

    httprequest

--- static/tomio.js
+++ static/tomio.js
@@ -16,6 +16,6 @@ btn.addEventListener("click", generovat)
 
 function generovat() {
     let words_count = parseInt(words_select.value)
-    let result = httpGet("https://www.piipovostranky.cz/visuals/tomiem_ipsum/" + String(words_count))
+    let result = httpGet("http://www.piipovostranky.cz/visuals/tomiem_ipsum/" + String(words_count))
     result_div.innerHTML = result
 }
